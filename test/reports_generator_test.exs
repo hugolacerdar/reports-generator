2ed0afd520329749e243de8fa1034c1c280318f7
Hugo <hugolacerdar@gmail.com>
2021-05-05 14:06:12 -0300

    Adding parallel version for the build fn

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -56,6 +56,71 @@ defmodule ReportsGeneratorTest do
     end
   end
 
+  describe "build_from_many/1" do
+    test "should build the report when a file list is provided" do
+      filenames = ["report_test.csv", "report_test.csv"]
+
+      response = ReportsGenerator.build_from_many(filenames)
+
+      expected =
+        {:ok,
+         %{
+           "foods" => %{
+             "açaí" => 2,
+             "churrasco" => 4,
+             "esfirra" => 6,
+             "hambúrguer" => 4,
+             "pastel" => 0,
+             "pizza" => 4,
+             "prato_feito" => 0,
+             "sushi" => 0
+           },
+           "users" => %{
+             "1" => 96,
+             "10" => 72,
+             "11" => 0,
+             "12" => 0,
+             "13" => 0,
+             "14" => 0,
+             "15" => 0,
+             "16" => 0,
+             "17" => 0,
+             "18" => 0,
+             "19" => 0,
+             "2" => 90,
+             "20" => 0,
+             "21" => 0,
+             "22" => 0,
+             "23" => 0,
+             "24" => 0,
+             "25" => 0,
+             "26" => 0,
+             "27" => 0,
+             "28" => 0,
+             "29" => 0,
+             "3" => 62,
+             "30" => 0,
+             "4" => 84,
+             "5" => 98,
+             "6" => 36,
+             "7" => 54,
+             "8" => 50,
+             "9" => 48
+           }
+         }}
+
+      assert response == expected
+    end
+
+    test "should return an error when a file list is not provided" do
+      response = ReportsGenerator.build_from_many("banana")
+
+      expected = {:error, "Please provide a list of strings"}
+
+      assert response == expected
+    end
+  end
+
   describe "fetch_higher/2" do
     test "when the option equals 'users', should return the user that spent the most" do
       filename = "report_test.csv"
